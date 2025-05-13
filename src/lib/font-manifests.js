2ccf95568aacec5f36ff202496feb5d267a91a2b
NECOdess <necodes@duck.com>
2025-05-13 21:31:52 +0200

    font: ...

--- src/lib/font-manifests.js
+++ src/lib/font-manifests.js
@@ -1,6 +1,6 @@
 export const fontData = {
 	commitmono: () => import('./fonts/commitmono/commitmono.json'),
-	lato: () => import('./fonts/space_mono/lato.json'),
+	lato: () => import('./fonts/lato/lato.json'),
 	space_mono: () => import('./fonts/space_mono/space_mono.json'),
 	ubuntu: () => import('./fonts/ubuntu/ubuntu.json')
 }
