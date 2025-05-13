a8286df08759daede9a1d6c00d95a4f26518ea65
NECOdess <necodes@duck.com>
2025-05-13 23:54:07 +0200

    font: new font - Montserrat; trying more font attributes

--- src/lib/font-manifests.js
+++ src/lib/font-manifests.js
@@ -1,6 +1,7 @@
 export const fontData = {
 	commitmono: () => import('./fonts/commitmono/commitmono.json'),
 	lato: () => import('./fonts/lato/lato.json'),
+	montserrat: () => import('./fonts/montserrat/montserrat.json'),
 	space_mono: () => import('./fonts/space_mono/space_mono.json'),
 	ubuntu: () => import('./fonts/ubuntu/ubuntu.json')
 }
---
+++ src/lib/fonts/montserrat/README.md
@@ -0,0 +1,6 @@
+> Montserrat is a geometric sans-serif typeface designed by Argentine graphic designer Julieta Ulanovsky and released in 2011.
+
+To use, copy the following link in your Bunny's Fonts page
+```html
+https://bunny.kat.tf/fonts/montserrat
+```
