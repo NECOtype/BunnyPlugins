12d1269938ca3c41cff1f39ffa14cbd47b40d880
NECOdess <necodes@duck.com>
2025-05-14 18:13:04 +0200

    svelte: update main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,6 +15,8 @@
 			<header>Fonts</header>
 			<ul>
 				<li><a href="/fonts/commit_mono">Commit Mono</a></li>
+				<li><a href="/fonts/lato">Lato</a></li>
+				<li><a href="/fonts/montserrat">Lato</a></li>
 				<li><a href="/fonts/space_mono">Space Mono</a></li>
 				<li><a href="/fonts/ubuntu">Ubuntu</a></li>
 			</ul>
