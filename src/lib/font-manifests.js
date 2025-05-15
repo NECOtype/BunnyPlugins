3ec060d091be4a152d8c18bdfda7c4f36fba6e4e
NECOdess <necodes@duck.com>
2025-05-15 20:32:06 +0200

    basic Astro app that serves font manifests

--- src/lib/font-manifests.js
+++
@@ -1,7 +0,0 @@
-export const fontData = {
-	commit_mono: () => import('./fonts/commit_mono/commit_mono.json'),
-	lato: () => import('./fonts/lato/lato.json'),
-	montserrat: () => import('./fonts/montserrat/montserrat.json'),
-	space_mono: () => import('./fonts/space_mono/space_mono.json'),
-	ubuntu: () => import('./fonts/ubuntu/ubuntu.json')
-}
